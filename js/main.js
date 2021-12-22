d24bda59d67802c3aa8f62c1042d7bc21ca942e0
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-23 00:28:54 +0900

    12.23/00:28

--- js/main.js
+++ js/main.js
@@ -1,2 +1,23 @@
 
 
+$(function(){
+    $("#next").click(function(){
+        $("#re_music_listen_slide").hide();
+        $("#re_music_listen_slide_2").show().css("display", "flex");
+        $("#prev").show().css("display", "block");
+        $(this).hide();
+    })
+
+    $("#prev").click(function(){
+        $("#re_music_listen_slide").show();
+        $("#re_music_listen_slide_2").hide().css("display", "none");
+        $("#next").show().css("display", "block");
+        $(this).hide();
+    })
+})
+
+$(function(){
+    $("#valley_society").click(function(){
+        $("#the_bottom_music_bar").show();
+    })
+})
