bd4c960d47a10fa26a074f97a52ba88d1a8ec9a5
fe2oz <93234748+fe2oz@users.noreply.github.com>
2021-12-23 17:42:25 +0900

    12.23/17:42

--- js/main.js
+++ js/main.js
@@ -14,10 +14,43 @@ $(function(){
         $("#next").show().css("display", "block");
         $(this).hide();
     })
+
+    $("#next1").click(function(){
+        $("#enjoy_music_listen_slide").hide();
+        $("#enjoy_music_listen_slide_2").show().css("display", "flex");
+        $("#prev1").show().css("display", "block");
+        $(this).hide();
+    })
+
+    $("#prev1").click(function(){
+        $("#enjoy_music_listen_slide").show();
+        $("#enjoy_music_listen_slide_2").hide().css("display", "none");
+        $("#next1").show().css("display", "block");
+        $(this).hide();
+    })
+
 })
 
 $(function(){
     $("#valley_society").click(function(){
         $("#the_bottom_music_bar").show();
+        $("#play_music").attr("src", "img/743894_pause_control_line_play_stop_icon.png").css("filter", "invert(1)");
+        $("#SOCIETY_music")[0].play();
     })
+
+    
 })
+
+// 재생, 정지 버튼 토글
+function imgToggle(){
+    var img = document.getElementById("play_music");
+    if(img.src.match("stop_icon")){
+        img.src = "img/211876_play_icon.png";
+    }else{
+        img.src = "img/743894_pause_control_line_play_stop_icon.png";
+    }
+}
+
+
+
+
