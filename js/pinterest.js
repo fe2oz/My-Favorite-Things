f7da5bb66b80513f56f23d04d77bc795288a9a43
fe2oz <93234748+fe2oz@users.noreply.github.com>
2022-01-04 17:31:48 +0900

    01.04/17:31

--- js/pinterest.js
+++ js/pinterest.js
@@ -3,29 +3,39 @@
 
 $(document).ready(function(){
     const pinOne = document.getElementById("citypop-pin");
-    const pinTwo = document.getElementById("pixel-art-pin");
+    const pinTwo = document.getElementById("pixelart-pin");
     const pinThree = document.getElementById("anime-pin");
-    const cityPopBg = document.getElementById("citypop");
-    const pixelArtBg = document.getElementById("pixel-art");
-    const animeBg = document.getElementById("anime");
+
 
     
 
     pinOne.addEventListener("click", function(){
-        cityPopBg.style.visibility = "visible";
-        pixelArtBg.style.visibility = "hidden";
-        animeBg.style.visibility = "hidden";
+        document.getElementById("citypop").style.visibility = "visible";
+        document.getElementById("pixelart").style.visibility = "hidden";
+        document.getElementById("anime").style.visibility = "hidden";
     });
 
     pinTwo.addEventListener("click", function(){
-        pixelArtBg.style.visibility = "visible";
-        cityPopBg.style.visibility = "hidden";
-        animeBg.style.visibility = "hidden";
+        document.getElementById("pixelart").style.visibility = "visible";
+        document.getElementById("citypop").style.visibility = "hidden";
+        document.getElementById("anime").style.visibility = "hidden";
     });
 
     pinThree.addEventListener("click", function(){
-        animeBg.style.visibility = "visible";
-        cityPopBg.style.visibility = "hidden";
-        pixelArtBg.style.visibility = "visible";
+        document.getElementById("anime").style.visibility = "visible";
+        document.getElementById("citypop").style.visibility = "hidden";
+        document.getElementById("pixelart").style.visibility = "hidden";
     });
+
+
+    
+    
 });
+
+
+$(function(){
+    $(".pinbox li").click(function(){
+        $(".pinbox li").removeClass("activeA");
+        $(this).addClass("activeA")
+    })
+})
